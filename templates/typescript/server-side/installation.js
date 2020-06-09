ad443f2d64fb07bd17104a43532c76d194f858f4
Shir Ohana <shir.o@pepperi.com>
2020-06-09 18:22:25 +0300

    default_installation_js  - Clarifications about the return object of the installation.js functions

--- templates/typescript/server-side/installation.js
+++ templates/typescript/server-side/installation.js
@@ -5,7 +5,7 @@ The return object format MUST contain the field 'success':
 
 If the result of your code is 'false' then return:
 {success:false, erroeMessage:{the reason why it is false}}
-The erroeMessage is importent! it will be written in the audit log and help the user to understand what happen
+The erroeMessage is importent! It will be written in the audit log and help the user to understand what happen
 */
 
 
