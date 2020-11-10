318aaa83a94eec0948ee22cb6c5cad5516cc56f3
alonnoy <alon.n@pepperi.com>
2020-11-10 12:37:15 +0200

    change templatePath

--- index.js
+++ index.js
@@ -145,9 +145,9 @@ const main = async() => {
         console.log('extracting zip file');
         await extract(zipFile, tmpPath);
 
-        const rootTemplatePath = tmpPath + '\\templates\\root\\';
-        const serverTemplatePath = tmpPath + '\\templates\\server-side\\' + serverSideTmp;
-        const clientTemplatePath = tmpPath + '\\templates\\client-side\\' + clientSideTmp + (clientSideVer ? '/' + clientSideVer : '' );
+        const rootTemplatePath = tmpPath + '\\create-addon-0.0.29\\templates\\root\\';
+        const serverTemplatePath = tmpPath + '\\create-addon-0.0.29\\templates\\server-side\\' + serverSideTmp;
+        const clientTemplatePath = tmpPath + '\\create-addon-0.0.29\\templates\\client-side\\' + clientSideTmp + (clientSideVer ? '/' + clientSideVer : '' );
 // \\create-addon-master
         if (!fs.existsSync(rootTemplatePath)) {
             throw new Error(`Template ${rootTemplatePath} doesn't exists`);
