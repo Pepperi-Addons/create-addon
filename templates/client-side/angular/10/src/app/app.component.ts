a5edafb61a50c08610797a3efa70f47dd69198b3
tomerp_pepperi <tomer.p@pepperi.com>
2020-11-12 15:04:44 +0200

    - Update Template for webpapp.

--- templates/client-side/angular/10/src/app/app.component.ts
+++ templates/client-side/angular/10/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomizationService, PepStyleType } from '@pepperi-addons/ngx-lib';
 
 @Component({
-  selector: 'addon-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'addon-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client-side';
+export class AppComponent implements OnInit {
+    
+    footerHeight: number;
+
+    showLoading = false;
+
+    constructor(public customizationService: CustomizationService) {
+    } 
+
+    ngOnInit() {
+        this.customizationService.setThemeVariables();
+
+        this.customizationService.footerHeight.subscribe(footerHeight => {
+            this.footerHeight = footerHeight;
+        });
+    }
+
+    getTopBarStyle() {
+        return document.documentElement.style.getPropertyValue(CustomizationService.STYLE_TOP_HEADER_KEY) as PepStyleType;
+    }
+
+    navigateHome() {
+        alert('Home');
+    }
+
+    getButtonClassName() {
+        return this.getTopBarStyle() === 'strong' ? 'keep-background-on-focus' : 'invert';
+    }
+
 }
--- templates/client-side/angular/10/src/app/modules/pepperi.module.ts
+++ templates/client-side/angular/10/src/app/modules/pepperi.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PepIconModule, PepIconRegistry,
-  pepIconSystemBolt,  pepIconNoImage
-} from '@pepperi-addons/ngx-lib/icon';
-import { PepNgxLibModule, AddonService, CustomizationService} from '@pepperi-addons/ngx-lib';
+import { HttpClient } from '@angular/common/http';
+
+import { PepNgxLibModule, AddonService, CustomizationService, FileService } from '@pepperi-addons/ngx-lib';
 import { PepAttachmentModule } from '@pepperi-addons/ngx-lib/attachment';
 import { PepCheckboxModule } from '@pepperi-addons/ngx-lib/checkbox';
 import { PepColorModule } from '@pepperi-addons/ngx-lib/color';
@@ -22,78 +21,157 @@ import { PepSizeDetectorModule } from '@pepperi-addons/ngx-lib/size-detector';
 import { PepTextareaModule } from '@pepperi-addons/ngx-lib/textarea';
 import { PepTextboxModule } from '@pepperi-addons/ngx-lib/textbox';
 import { PepListModule } from '@pepperi-addons/ngx-lib/list';
-import { HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
-}
-
-const pepperiComponentsModules = [
-  PepAttachmentModule,
-  PepCheckboxModule,
-  PepColorModule,
-  PepDateModule,
-  PepGroupButtonsModule,
-  PepImageModule,
-  PepImagesFilmstripModule,
-  PepListModule,
-  PepCheckboxModule,
-  PepInternalButtonModule,
-  PepMenuModule,
-  PepQuantitySelectorModule,
-  PepRichHtmlTextareaModule,
-  PepSelectModule,
-  PepSeparatorModule,
-  PepSignatureModule,
-  PepSizeDetectorModule,
-  PepTextareaModule,
-  PepTextboxModule,
-  PepIconModule
+import {PepIconModule, PepIconRegistry,
+    pepIconSystemBolt,
+    pepIconNoImage,
+    pepIconArrowTwoWaysVerT,
+    pepIconArrowDown,
+    pepIconArrowUp,
+    pepIconArrowRightAlt,
+    pepIconArrowLeftAlt,
+    pepIconArrowDownAlt,
+    pepIconArrowUpAlt,
+    pepIconNumberNumber,
+    pepIconNumberPlus,
+    pepIconSystemBin,
+    pepIconSystemEdit,
+    pepIconSystemClose,
+    pepIconSystemFilter,
+    pepIconSystemMenu,
+    pepIconSystemHome,
+    pepIconSystemSettings,
+    pepIconSystemQuestion,
+    pepIconSystemAvatar,
+    pepIconSystemDoor,
+    pepIconSystemPrint,
+    pepIconSystemSearch,
+    pepIconSystemSpinner,
+    pepIconSystemInfo,
+    pepIconShoppingCart,
+    pepIconTimeCal,
+    pepIconViewCardLg,
+    pepIconViewCardMd,
+    pepIconViewCardSm,
+    pepIconViewTable,
+    pepIconViewMatrix,
+    pepIconViewLine
+} from '@pepperi-addons/ngx-lib/icon';
+const pepIcons = [
+    pepIconSystemBolt,
+    pepIconNoImage,
+    pepIconArrowTwoWaysVerT,
+    pepIconArrowDown,
+    pepIconArrowUp,
+    pepIconArrowRightAlt,
+    pepIconArrowLeftAlt,
+    pepIconArrowDownAlt,
+    pepIconArrowUpAlt,
+    pepIconNumberNumber,
+    pepIconNumberPlus,
+    pepIconSystemBin,
+    pepIconSystemEdit,
+    pepIconSystemClose,
+    pepIconSystemFilter,
+    pepIconSystemMenu,
+    pepIconSystemHome,
+    pepIconSystemSettings,
+    pepIconSystemQuestion,
+    pepIconSystemAvatar,
+    pepIconSystemDoor,
+    pepIconSystemPrint,
+    pepIconSystemSearch,
+    pepIconSystemSpinner,
+    pepIconSystemInfo,
+    pepIconShoppingCart,
+    pepIconTimeCal,
+    pepIconViewCardLg,
+    pepIconViewCardMd,
+    pepIconViewCardSm,
+    pepIconViewTable,
+    pepIconViewMatrix,
+    pepIconViewLine
 ];
 
+const pepperiComponentsModules = [
+    PepAttachmentModule,
+    PepCheckboxModule,
+    PepColorModule,
+    PepDateModule,
+    PepGroupButtonsModule,
+    PepImageModule,
+    PepImagesFilmstripModule,
+    PepListModule,
+    PepCheckboxModule,
+    PepInternalButtonModule,
+    PepMenuModule,
+    PepQuantitySelectorModule,
+    PepRichHtmlTextareaModule,
+    PepSelectModule,
+    PepSeparatorModule,
+    PepSignatureModule,
+    PepSizeDetectorModule,
+    PepTextareaModule,
+    PepTextboxModule,
+    PepIconModule
+];
+
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+
+// export function createTranslateLoader(http: HttpClient) {
+//    return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+// }
+
+export function createTranslateLoader(http: HttpClient, fileService: FileService) {
+    const translationsPath: string = fileService.getAssetsTranslationsPath();
+
+    return new MultiTranslateHttpLoader(http, [
+        {prefix: translationsPath, suffix: '.json'},
+        {prefix: '/assets/i18n/', suffix: '.json'},
+    ]);
+}
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PepNgxLibModule,
-    pepperiComponentsModules,
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: createTranslateLoader,
-          deps: [HttpClient]
-      }
-    })
-  ],
-  exports: [
-    PepNgxLibModule,
-    pepperiComponentsModules
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        PepNgxLibModule,
+        pepperiComponentsModules,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient, FileService]
+            }
+        })
+    ],
+    exports: [
+        PepNgxLibModule,
+        pepperiComponentsModules
+    ]
 })
 export class PepUIModule {
 
-  constructor(
-        translate: TranslateService,
-        private pepperiIconRegistry: PepIconRegistry
-    ) {
-      this.pepperiIconRegistry.registerIcons([
-        pepIconSystemBolt,
-        pepIconNoImage
-    ]);
+    constructor(
+          translate: TranslateService,
+          private pepperiIconRegistry: PepIconRegistry
+      ) {
+        
+        this.pepperiIconRegistry.registerIcons(pepIcons);
 
-      let userLang = 'en';
-      translate.setDefaultLang(userLang);
-      userLang = translate.getBrowserLang().split('-')[0]; // use navigator lang if available
+        let userLang = 'en';
+        translate.setDefaultLang(userLang);
+        userLang = translate.getBrowserLang().split('-')[0]; // use navigator lang if available
 
-      if (location.href.indexOf('userLang=en') > -1) {
-        userLang = 'en';
+        if (location.href.indexOf('userLang=en') > -1) {
+            userLang = 'en';
+        }
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use(userLang).subscribe((res: any) => {
+            // In here you can put the code you want. At this point the lang will be loaded
+        });
     }
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-      translate.use(userLang).subscribe((res: any) => {
-        // In here you can put the code you want. At this point the lang will be loaded
-    });
-    }
- }
+}
