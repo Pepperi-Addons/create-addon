ed07d0223acba39d0432d7ef47e9cb0a1f795407
tomerp_pepperi <tomer.p@pepperi.com>
2021-12-08 19:13:45 +0200

    - Some declarations fix for the spa.

--- templates/client-side/angular/blankAddon/src/main.ts
+++ templates/client-side/angular/blankAddon/src/main.ts
@@ -1,1 +1,27 @@
-import('./bootstrap').catch(err => console.error(err));
+import { enableProdMode, NgZone } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router } from '@angular/router';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa-props';
+
+if (environment.production) {
+    enableProdMode();
+}
+
+let lifecycles = singleSpaAngular({
+    bootstrapFunction: singleSpaProps => {
+        singleSpaPropsSubject.next(singleSpaProps);
+        return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+    },
+    template: '<addon-root />',
+    Router,
+    NgZone
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
+
+// import('./bootstrap').catch(err => console.error(err));
