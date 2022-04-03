562ae94956ad7fe9eb39277dfa1d57e92c522f1f
tomerp_pepperi <tomer.p@pepperi.com>
2022-04-03 14:03:42 +0300

    - Change actionUuid to use client["ActionUUID"] instead of client.AddonUUID.

--- templates/server-side/typescript/my.service.ts
+++ templates/server-side/typescript/my.service.ts
@@ -11,7 +11,7 @@ class MyService {
             token: client.OAuthAccessToken,
             addonUUID: client.AddonUUID,
             addonSecretKey: client.AddonSecretKey,
-            actionUUID: client.AddonUUID
+            actionUUID: client["ActionUUID"]
         });
     }
 
