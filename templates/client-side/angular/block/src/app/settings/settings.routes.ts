50e10e32e380e0b285d0f70574005ef2260044ae
tomerp_pepperi <tomer.p@pepperi.com>
2022-09-15 15:33:42 +0300

    - Fix duplicate settings

--- templates/client-side/angular/block/src/app/settings/settings.routes.ts
+++ templates/client-side/angular/block/src/app/settings/settings.routes.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SettingsComponent } from './settings.component';
+// import { SettingsComponent } from './settings.component';
 
 const routes: Routes = [
     {
         path: ':settingsSectionName/:addonUUID/:slugName',
-        component: SettingsComponent,
+        // component: SettingsComponent,
         children: [
             {
                 path: ':form_key',
