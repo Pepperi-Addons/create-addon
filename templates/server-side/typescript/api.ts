7393583e6921b211a7ef249eda2d67ae402e00ba
Saar Guttman <saar.g@pepperi.com>
2023-04-13 10:59:35 +0300

    Disable camelcase in server-side api filed

--- templates/server-side/typescript/api.ts
+++ templates/server-side/typescript/api.ts
@@ -1,3 +1,7 @@
+// The comment below disables the eslint camelcase rule for the whole file.
+// You can copy it to other files providing endpoints to your addon.
+/* eslint camelcase: 0 */
+
 import { Client, Request } from '@pepperi-addons/debug-server'
 
 export async function test(client: Client, request: Request) {
