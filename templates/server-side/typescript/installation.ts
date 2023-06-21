6591aa11d89b1058785d8ba4debe80640b571d0a
Saar Guttman <saar.g@pepperi.com>
2023-06-21 14:14:14 +0300

    Run lint on entire project

--- templates/server-side/typescript/installation.ts
+++ templates/server-side/typescript/installation.ts
@@ -1,4 +1,4 @@
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /*
 The return object format MUST contain the field 'success':
 {success:true}
@@ -8,10 +8,16 @@ If the result of your code is 'false' then return:
 The error Message is important! it will be written in the audit log and help the user to understand what happen
 */
 
+interface AddonAPISyncResult {
+    success: boolean;
+    errorMessage?: string;
+    resultObject?: unknown;
+}
+
 import { Client, Request } from '@pepperi-addons/debug-server';
 import { RelationsService } from './services/relations.service';
 
-export async function install(client: Client, request: Request): Promise<any> {
+export async function install(client: Client, request: Request): Promise<AddonAPISyncResult> {
     try {
         const service = new RelationsService(client);
         await service.upsertRelations();
@@ -22,14 +28,14 @@ export async function install(client: Client, request: Request): Promise<any> {
     return { success: true, resultObject: {} };
 }
 
-export async function uninstall(client: Client, request: Request): Promise<any> {
-    return {success:true,resultObject:{}}
+export async function uninstall(client: Client, request: Request): Promise<AddonAPISyncResult> {
+    return {success: true, resultObject: {}}
 }
 
-export async function upgrade(client: Client, request: Request): Promise<any> {
-    return {success:true,resultObject:{}}
+export async function upgrade(client: Client, request: Request): Promise<AddonAPISyncResult> {
+    return {success: true, resultObject: {}}
 }
 
-export async function downgrade(client: Client, request: Request): Promise<any> {
-    return {success:true,resultObject:{}}
+export async function downgrade(client: Client, request: Request): Promise<AddonAPISyncResult> {
+    return {success: true, resultObject: {}}
 }
